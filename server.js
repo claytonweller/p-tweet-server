79f75557ea3a35c5c4eb8f75cd5adaedb21f58d5
unknown <clayton.weller@gmail.com>
2018-07-17 13:09:10 -0600

    Removed node-fetch.

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
 'use strict'
 
-// const fetch = require('node-fetch')
 const express = require('express');
-const bodyParser =require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const request = require("request");
 
-
 const twitter_api = 'https://api.twitter.com/1.1/search/tweets.json';
-const bearer_token = 'AAAAAAAAAAAAAAAAAAAAAEUl6wAAAAAAYctnbkuR1XL5CNjrPTBRON%2FmFfM%3D3kOOQjOZg0KpGNZyajj3dh7gPQFjj0gmYKdveohBz7TdTRlysZ'; 
+const bearer_token = 'AAAAAAAAAAAAAAAAAAAAAEUl6wAAAAAAYctnbkuR1XL5CNjrPTBRON%2FmFfM%3D3kOOQjOZg0KpGNZyajj3dh7gPQFjj0gmYKdveohBz7TdTRlysZ';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 //This pings the Heroku server immediately upon page load.
-app.get('/wakeUp/', function (req, res){
+app.get('/wakeUp/', function (req, res) {
   res.send('RRRRRR')
 })
 
 //This gets info from the Twitter search API.
-app.post('/', async function(req, res){
+app.post('/', async function (req, res) {
   // First we search with the topic they user entered
   let response = await twitterCall(req.body.search)
   // If it returns any tweets then we're good.
-  if(typeof response.statuses[0] === 'object'){
+  if (typeof response.statuses[0] === 'object') {
     res.send(response)
-  // If not we just get some 'perfect' tweets instead
-  }else{
+    // If not we just get some 'perfect' tweets instead
+  } else {
     response = await twitterCall('perfect')
     res.send(response)
   }
-  
-});
+})
 
-const twitterCall = (topic) =>{
+//At first I attempted to use node-fetch. 
+//But because of my function above having a recursive 2nd call
+//I use promises and thre request module instead.
+const twitterCall = (topic) => {
   const options = {
     method: 'GET',
-    url: twitter_api, 
+    url: twitter_api,
     qs: {
       q: topic,
-      'result_type':'popular',
-      'tweet_mode':'extended',
-      lang:'en',
+      'result_type': 'popular',
+      'tweet_mode': 'extended',
+      lang: 'en',
       count: 10,
     },
     json: true,
@@ -50,8 +50,8 @@ const twitterCall = (topic) =>{
       "Authorization": "Bearer " + bearer_token
     }
   };
-  return new Promise(function(resolve, reject) {
-    request.get(options, function(err, resp, body) {
+  return new Promise(function (resolve, reject) {
+    request.get(options, function (err, resp, body) {
       if (err) {
         reject(err);
       } else {
@@ -60,9 +60,9 @@ const twitterCall = (topic) =>{
     })
   })
 
-} 
+}
 
 const port = process.env.PORT || process.env.VCAP_APP_PORT || 3005;
-app.listen(port, function() {
- console.log(`app is running on port ${port}`);
+app.listen(port, function () {
+  console.log(`app is running on port ${port}`);
 });
